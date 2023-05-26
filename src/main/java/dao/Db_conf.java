c330ea11e90ffde49cf82a8295736901519e9103
manabu <miyamotoma678@gmail.com>
2023-05-26 16:56:42 +0900

    データベースのドライバの読み込み部分を修正。

--- src/main/java/dao/Db_conf.java
+++ src/main/java/dao/Db_conf.java
@@ -1,9 +1,11 @@
 package dao;
 
 public class Db_conf {
+	private final String D_CLASS = "org.h2.Driver";
 	private final String JDBC_URL = "jdbc:h2:tcp://localhost/~/blog";
 	private final String DB_USER = "manabu";
 	private final String DB_PASS = "Mpa0515";
+	public String getD_CLASS() { return D_CLASS; }
 	public String getJDBC_URL() { return JDBC_URL; }
 	public String getDB_USER() { return DB_USER; }
 	public String getDB_PASS() { return DB_PASS; }	
--- src/main/java/dao/ReadJDBC.java
+++ src/main/java/dao/ReadJDBC.java
@@ -3,8 +3,10 @@ package dao;
 public class ReadJDBC {
 	public void read() {
 		// JDBCドライバを読み込む
+		Db_conf conf = new Db_conf();
+		String driver = conf.getD_CLASS();
 		try {
-			Class.forName("org.h2.Driver");
+			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
 			throw new IllegalStateException("JDBCドライバを読み込めませんでした");
 		}
